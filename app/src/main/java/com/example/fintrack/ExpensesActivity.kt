b15ba7f2053476d102dd59d00d7bf45432197772
Alexandre Lopes Terço <alexandreterco@gmail.com>
2024-05-20 14:46:32 -0300

    codigo para criaçao de botao e alerta, e o spinner

--- app/src/main/java/com/example/fintrack/ExpensesActivity.kt
+++ app/src/main/java/com/example/fintrack/ExpensesActivity.kt
@@ -1,6 +1,10 @@
 package com.example.fintrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class ExpensesActivity : AppCompatActivity() {
@@ -10,6 +14,36 @@ class ExpensesActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val categorySpinner: Spinner = findViewById(R.id.categorySpinner)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.category_array,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        categorySpinner.adapter=adapter
+
     }
+
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Tem certeza que deseja cancelar inclusão?")
+            .setCancelable(false)
+            .setPositiveButton("Sim") { dialog, id ->
+                val intent = Intent(this, FinTrackActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+                finish()
+            }
+            .setNegativeButton("Não"){dialog, id ->
+                dialog.dismiss()
+            }
+        val alerta = builder.create()
+        alerta.show()
+
+    }
+
 
 }
